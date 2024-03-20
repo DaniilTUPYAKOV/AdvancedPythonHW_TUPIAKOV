9ee20149f85c0c9d7345e2fb3ce650aea4c49186
DaniilTUPYAKOV <tupyakov.daniil@gmail.com>
2024-03-20 17:25:48 +0300

    fully add

--- hw_3/main_3_3.py
+++ hw_3/main_3_3.py
@@ -1,7 +1,18 @@
 import numpy as np
 
 
-class Matrix():
+class HashMixin:
+
+    def __hash__(self) -> int:
+        my_hash = 0
+        for elem in str(self):
+            my_hash += ord(elem)
+        return my_hash
+
+
+class Matrix(HashMixin):
+    
+    __chache = {}
 
     def __init__(self, matrix):
         if not isinstance(matrix, list):
@@ -10,7 +21,8 @@ class Matrix():
             raise ValueError("All rows in '_matrix' must have the same length")
         for elem in matrix:
             if len(elem) != len(matrix[0]):
-                raise ValueError("All rows in '_matrix' must have the same length")
+                raise ValueError(
+                    "All rows in '_matrix' must have the same length")
         self._matrix = matrix
         self._rows_num = len(matrix)
         self._cols_num = len(matrix[0])
@@ -31,7 +43,8 @@ class Matrix():
         if self._rows_num != other._rows_num or self._cols_num != other._cols_num:
             raise ValueError("Matrices are not the same size!")
         result = [
-            [self._matrix[i][j] + other._matrix[i][j] for j in range(self._cols_num)]
+            [self._matrix[i][j] + other._matrix[i][j]
+                for j in range(self._cols_num)]
             for i in range(self._rows_num)
         ]
         return Matrix(result)
@@ -46,36 +59,61 @@ class Matrix():
                     so cannot be element-by-element multiplied!"""
             )
         result = [
-            [self._matrix[i][k] * other._matrix[i][k] for k in range(self._cols_num)]
+            [self._matrix[i][k] * other._matrix[i][k]
+                for k in range(self._cols_num)]
             for i in range(self._rows_num)
         ]
         return Matrix(result)
 
     @check_arg_type
     def __matmul__(self, other: "Matrix"):
+
+        chache = hash(self) * 4 * hash(other)
+        if chache in self.__chache:
+            return Matrix(self.__chache[chache])
+
         if self._cols_num != other._rows_num or self._rows_num != other._cols_num:
             raise ValueError("Matrices cannot be multiplied.")
 
         result = [
-            [sum(a * b for a, b in zip(A_row, B_col)) for B_col in zip(*other._matrix)]
+            [sum(a * b for a, b in zip(A_row, B_col))
+             for B_col in zip(*other._matrix)]
             for A_row in self._matrix
         ]
 
+        self.__chache[chache] = result
         return Matrix(result)
+
+    def __str__(self) -> str:
+        return "\n".join([" ".join([str(elem) for elem in row]) for row in self._matrix])
 
 
 np.random.seed(0)
-matrix_a = Matrix(np.random.randint(0, 10, (10, 10)).tolist())
-matrix_b = Matrix(np.random.randint(0, 10, (10, 10)).tolist())
+A = Matrix(np.random.randint(0, 10, (10, 10)).tolist())
+C = Matrix(np.array(A._matrix).transpose().tolist())
+
+B = Matrix(np.random.randint(0, 10, (10, 10)).tolist())
+D = B
+
+if ((hash(A) == hash(C)) and (A != C) and (B == D) and (A @ B != C @ D)):
+
+    AB = A @ B
+    CD = C @ D
+
+    def save_result_mat(mat: Matrix, filename: str):
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write("\n".join([" ".join(map(str, row)) for row in mat._matrix]))
+
+    def save_result_text(text: str, prefix: str, filename: str):
+        with open(filename, "+a", encoding="utf-8") as file:
+            file.write(prefix + " " + text + "\n")
 
 
-def save_result(mat: Matrix, filename: str):
-    with open(filename, "w", encoding="utf-8") as file:
-        file.write("\n".join([" ".join(map(str, row)) for row in mat]))
-
-
-save_result(matrix_a, "matrix_a.txt")
-save_result(matrix_b, "matrix_b.txt")
-save_result(matrix_a + matrix_b, "matrix+.txt")
-save_result(matrix_a * matrix_b, "matrix_mult.txt")
-save_result(matrix_a @ matrix_b, "matrix@.txt")
+    save_result_mat(A, "A.txt")
+    save_result_mat(B, "B.txt")
+    save_result_mat(C, "C.txt")
+    save_result_mat(D, "D.txt")
+    save_result_mat(AB, "AB.txt")
+    # save_result_mat(CD, "CD.txt")
+    save_result_text(str(hash(AB)), "Hash AB =", "hash.txt")
+    save_result_text(str(hash(CD)), "Hash CD =", "hash.txt")
