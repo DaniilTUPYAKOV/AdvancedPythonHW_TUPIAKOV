b305da3fd07aa1c8fd423584d4de06ddea52e5c0
DaniilTUPYAKOV <tupyakov.daniil@gmail.com>
2024-04-02 17:09:03 +0300

    add thread to hw4.3

--- hw_4/main_4_3.py
+++ hw_4/main_4_3.py
@@ -1,14 +1,16 @@
-
 import multiprocessing
+import threading
 import time
 import codecs
 
+
 def get_timestamp():
-    """ Return string of current time """
+    """Return string of current time"""
     return time.strftime("%H:%M:%S", time.localtime())
 
+
 def process_a_func(queue: multiprocessing.Queue, conn_a_to_b):
-    """ Implementation of process A 
+    """Implementation of process A
 
     Args:
         queue (multiprocessing.Queue): Queue from Main process to A process
@@ -20,8 +22,9 @@ def process_a_func(queue: multiprocessing.Queue, conn_a_to_b):
         conn_a_to_b.send(message)
         time.sleep(5)
 
+
 def process_b_func(conn_b_to_a, conn_b_to_main):
-    """ Implementation of process B
+    """Implementation of process B
 
     Args:
         conn_b_to_a (PipeConnection): Pipe connection from process B to process A
@@ -32,6 +35,21 @@ def process_b_func(conn_b_to_a, conn_b_to_main):
         message = conn_b_to_a.recv()
         encoded_message = codecs.encode(message, "rot-13")
         conn_b_to_main.send(encoded_message)
+
+
+def stdout_thread_method(parent_conn_main_to_b, user_exit_event: threading.Event):
+    """ Body of Tread for printing received frm B messages"""
+    try:
+        while not user_exit_event.is_set():
+            if parent_conn_main_to_b.poll():
+                received_message = parent_conn_main_to_b.recv()
+                print(
+                    f"\rEncoded message: '{received_message}' received at {get_timestamp()}"
+                    + "\nEnter a message: ",
+                    end=""
+                )
+    except KeyboardInterrupt:
+        return
 
 
 if __name__ == "__main__":
@@ -51,20 +69,20 @@ if __name__ == "__main__":
     )
     process_b.start()
 
+    user_exit = threading.Event()
+    stdout_thread = threading.Thread(
+        target=stdout_thread_method, args=(parent_conn_main_to_b, user_exit)
+    )
+    stdout_thread.start()
+
     try:
         while True:
             main_message = input("Enter a message: ")
-            print(
-                f"Message '{main_message}' was entered at {get_timestamp()}"
-            )
-
+            print(f"Message '{main_message}' was entered at {get_timestamp()}")
             queue_main_to_a.put(main_message)
-            received_message = parent_conn_main_to_b.recv()
-            print(
-                f"Encoded message: '{received_message}' received at {get_timestamp()}"
-            )
 
     except KeyboardInterrupt:
+        user_exit.set()
         process_a.terminate()
         process_b.terminate()
         print("Applications stopped")
